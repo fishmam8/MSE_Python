446cc24cb3f45fbbbf09b309171a3b299222e6a4
fishmam8 <74127711+fishmam8@users.noreply.github.com>
2020-12-09 19:21:17 +0900

    Update ex100.py

--- ex100.py
+++ ex100.py
@@ -1,4 +1,4 @@
 date = ['09/05', '09/06', '09/07', '09/08', '09/09']
 close_price = [10500, 10300, 10100, 10800, 11000]
-close_dict = dict(zip(date,close_price))
+close_dict = dict(zip(date,close_price))             #date를 key로, close_price를 value로 하는 딕셔너리 생성.
 print(close_dict)
